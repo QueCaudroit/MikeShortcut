409a10f4fbee3106bcb25002d6576ae04ae042cf
Quentin Caudroit <quentin.caudroit@free.fr>
2022-07-22 18:25:10 +0200

    sets for path to recompute and iterate normal path based on distance

--- main.py
+++ main.py
@@ -17,24 +17,34 @@ def find_shortest_path(n_intersection, shortcuts):
 def find_shortest_path_shortcut_first(n_intersection, shortcuts):
     path_length = [i for i in range(n_intersection)]
     optimal = False
-    modified_paths = [i for i in range(n_intersection)]
+    modified_paths = set([i for i in range(n_intersection)])
     while not optimal:
         optimal = True
-        shortcut_modified_paths = []
-        standard_modified_path = []
+        shortcut_modified_paths = set()
+        standard_modified_path = set()
         for modified_path in modified_paths:
             if path_length[modified_path] + 1 < path_length[shortcuts[modified_path]]:
                 optimal = False
                 path_length[shortcuts[modified_path]] = path_length[modified_path] + 1
-                shortcut_modified_paths.append(shortcuts[modified_path])
-        for intersection_to in range(n_intersection):
-            for intersection_from in shortcut_modified_paths:
+                shortcut_modified_paths.add(shortcuts[modified_path])
+        for intersection_from in shortcut_modified_paths:
+            for intersection_to in range(intersection_from - 1, 0, -1):
                 length = abs(intersection_to - intersection_from)
                 if path_length[intersection_to] > path_length[intersection_from] + length:
                     optimal = False
                     path_length[intersection_to] = path_length[intersection_from] + length
-                    standard_modified_path.append(intersection_to)
-        modified_paths = standard_modified_path + shortcut_modified_paths
+                    standard_modified_path.add(intersection_to)
+                else:
+                    break
+            for intersection_to in range(intersection_from +1, n_intersection):
+                length = abs(intersection_to - intersection_from)
+                if path_length[intersection_to] > path_length[intersection_from] + length:
+                    optimal = False
+                    path_length[intersection_to] = path_length[intersection_from] + length
+                    standard_modified_path.add(intersection_to)
+                else:
+                    break
+        modified_paths = standard_modified_path | shortcut_modified_paths
     return path_length
 
 if __name__ == "__main__":
